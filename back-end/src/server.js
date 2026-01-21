c2bef202c821efa08855b1ee6c2a8e6d4e7ce124
simon@gotoeis.com <simon@gotoeis.com>
2026-01-21 17:08:30 -0500

    part three done

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,43 +1,85 @@
 import express from 'express'
-
-const articleInfo = [
-  { name: 'learn-react', upvotes: 10, comments: [] },
-  { name: 'learn-node', upvotes: 5, comments: [] },
-  { name: 'my-thoughts-on-react', upvotes: 7, comments: [] }
-]
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-  const articleName = req.params.name
-  const article = articleInfo.find((a) => a.name === articleName)
+let db;
 
+async function connectToDB(){
+   const uri = 'mongodb://127.0.0.1:27017';
+  const client = new MongoClient(uri, {  
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,  
+    } 
+  }); 
+
+  await client.connect();
+  
+  db = client.db('full-stack-react-db');
+}
+
+
+app.get('/api/articles/:name', async (req, res) => {
+  const articleName = req.params.name
+  const article = await db.collection('articles').findOne({ name: articleName }); 
   if (article) {
-    article.upvotes += 1
-    res
-      .status(200)
-      .json({
-        message: `The article ${articleName} now has ${article.upvotes} upvotes!`
-      })
+    res.status(200).json(article)
+  }
+  else {
+    res.status(404).json({ message: 'Article not found' })
+  }   
+})
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  const articleName = req.params.name
+  const updatedArticle = await db.collection('articles').findOneAndUpdate(
+    { name: articleName },
+    { $inc: { upvotes: 1 } },
+    { returnDocument: 'after' } 
+  )
+
+  if (updatedArticle) {
+
+    res.status(200).json({
+      message: `The article ${articleName} now has ${updatedArticle.upvotes} upvotes!`
+    })
   } else {
     res.status(404).json({ message: 'Article not found' })
   }
 })
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
   const articleName = req.params.name
-  const { postedBy, text } = req.body         
-  const article = articleInfo.find((a) => a.name === articleName)
-  if (article) {
-    article.comments.push({ postedBy, text })
-    res.status(200).json(article)
-  } else {   
+  const { postedBy, text } = req.body
+  const updatedArticle = await db
+    .collection('articles')
+    .findOneAndUpdate(
+      { name: articleName },
+      { $push: { comments: { postedBy, text } } },
+      { returnDocument: 'after' }
+    )
+
+  if (updatedArticle) {
+    res.status(200).json(updatedArticle)
+  } else {
     res.status(404).json({ message: 'Article not found' })
   }
 })
 
-app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080')
-})
+async function startServer() {
+  try {
+    await connectToDB();      
+    app.listen(8080, () => {
+      console.log('Server is running on http://localhost:8080')
+    } );
+  } catch (error) {
+    console.error('Failed to connect to the database', error);
+  }
+}
+startServer();
+
+
